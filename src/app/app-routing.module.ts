608c630e8bb22940c5d7a463d562f7a79037e582
jimboux <100678513+jimboux@users.noreply.github.com>
2023-07-03 12:01:37 +0200

    adding padding responsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,20 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LegalmentionsComponent } from './pages/legalmentions/legalmentions.component';
 import { CguComponent } from './pages/cgu/cgu.component';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { ReflexologieComponent } from './pages/reflexologie/reflexologie.component';
+import { AproposComponent } from './pages/apropos/apropos.component';
+import { SeancesComponent } from './pages/seances/seances.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   { path: 'mentions-legales', component: LegalmentionsComponent},
   { path: 'CGU', component: CguComponent},
+  { path: 'accueil', component: AccueilComponent},
+  { path: 'reflexologie', component: ReflexologieComponent},
+  { path: 'a-propos', component: AproposComponent},
+  { path: 'seances', component: SeancesComponent},
+  { path: 'contact', component: ContactComponent},
 ];
 
 @NgModule({
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  burgerVisible: boolean = false;
+  burgerVisible: boolean = true;
 
   burgerToggle() {
     this.burgerVisible = !this.burgerVisible;
