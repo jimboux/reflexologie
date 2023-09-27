db6d4c6d4992b4d3ef93a3aff94ec56b12e5e6fe
jimboux <100678513+jimboux@users.noreply.github.com>
2023-09-27 11:46:52 +0200

    adding meta

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +10,16 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
   showSubMenu = false;
   burgerVisible: boolean = true;
+
+  constructor(private router: Router) {}
+
+  // ngOnInit(){
+  //   this.router.events.pipe(filter(event => event instanceof
+  //     NavigationEnd)).subscribe(()=> {
+  //       window.scrollTo(0, 0);
+  //     })
+  // } 
+  // a faire pour le bouton pour arriver en hate de la page
   
   showMenu() {
     this.showSubMenu = !this.showSubMenu;
--- src/app/pages/apropos/apropos.component.ts
+++ src/app/pages/apropos/apropos.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-apropos',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./apropos.component.scss']
 })
 export class AproposComponent {
+  constructor(private router: Router) {}
 
+  ngOnInit(){
+    this.router.events.pipe(filter(event => event instanceof
+      NavigationEnd)).subscribe(()=> {
+        window.scrollTo(0, 0);
+      })
+  } 
+  
 }
