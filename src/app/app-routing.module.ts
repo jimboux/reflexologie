f2d8b6c133c273d6abc478ccf47e7c1e7f062f42
jimboux <100678513+jimboux@users.noreply.github.com>
2024-04-04 16:24:40 +0200

    loader en place et style de la page formulaire envoyé fait

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LegalmentionsComponent } from './pages/legalmentions/legalmentions.component';
 import { CguComponent } from './pages/cgu/cgu.component';
@@ -9,6 +9,7 @@ import { SeancesComponent } from './pages/seances/seances.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ConfidentialPoliticComponent } from './pages/confidential-politic/confidential-politic.component';
 import { Page404Component } from './pages/page404/page404.component';
+import { FormSentComponent } from './pages/form-sent/form-sent.component';
 
 const routes: Routes = [
   { path: '', component: AccueilComponent },
@@ -17,14 +18,11 @@ const routes: Routes = [
   { path: 'CGU', component: CguComponent },
   { path: 'reflexologie', component: ReflexologieComponent },
   { path: 'reflexologie#presentation', component: ReflexologieComponent },
-
+  { path: 'formulaire-envoyé', component: FormSentComponent },
   { path: 'a-propos', component: AproposComponent },
   { path: 'seances', component: SeancesComponent },
   { path: 'contact', component: ContactComponent },
-  {
-    path: 'politique-confidentialite',
-    component: ConfidentialPoliticComponent,
-  },
+  { path: 'politique-confidentialite', component: ConfidentialPoliticComponent },
   { path: '**', component: Page404Component },
 ];
 
