f60a63aadf58e9a6cc245be0cf00f452b620e6bb
Jimmy Cuogo <jimmy.cuogo@sncf.fr>
2024-07-30 13:24:29 +0200

    pwet

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,20 +8,6 @@ import { Router } from '@angular/router';
 export class HeaderComponent {
   showSubMenu = false;
   burgerVisible: boolean = true;
-
-  constructor(private router: Router) {}
-
-  // ngOnInit(){
-  //   this.router.events.pipe(filter(event => event instanceof
-  //     NavigationEnd)).subscribe(()=> {
-  //       window.scrollTo(0, 0);
-  //     })
-  // } 
-  // a faire pour le bouton pour arriver en hate de la page
-  
-  showMenu() {
-    this.showSubMenu = !this.showSubMenu;
-  }
 
   burgerToggle() {
     this.burgerVisible = !this.burgerVisible;
